fa8ee053318ad203eae55c397bb8e131b5c1a651
Nikita Ustiuzhanin <zaebumba@gmail.com>
2020-09-11 09:20:11 +0700

    Inital commit

---
+++ app.py
@@ -0,0 +1,72 @@
+import logging
+import os
+import time
+
+import requests
+import telegram
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+
+
+def parse_homework_status(homework):
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        return "Can't find homewwork name."
+
+    homework_status = homework.get('status')
+    if homework_status is None:
+        return "Can't find homework status."
+
+    if homework_status == 'rejected':
+        verdict = "Rejected. Need to check and fix mistakes."
+    else:
+        verdict = "Approved. All good. Go on."
+    return f'Homework is checked "{homework_name}"!\n\n{verdict}'
+
+
+def get_homework_statuses(current_timestamp):
+    if current_timestamp is None:
+        current_timestamp = int(time.time() - 300)
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(URL, headers=headers, params=payload)
+    except requests.exceptions.RequestException as e:
+        logging.basicConfig(filename='logger.log', level=logging.DEBUG)
+        logging.debug(f'{e} during request to API.')
+    else:
+        return homework_statuses.json()
+
+
+def send_message(message):
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
+
+
+def main():
+    current_timestamp = int(time.time())
+
+    while True:
+        try:
+            new_homework = get_homework_statuses(current_timestamp)
+            if new_homework.get('homeworks'):
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get('current_date')
+            time.sleep(300)
+
+        except Exception as e:
+            print(f'Bot crashed with: {e}')
+            time.sleep(5)
+            continue
+
+
+if __name__ == '__main__':
+    main()
